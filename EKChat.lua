8e519f918b55602b074e0f47b761f8a2b9286ccf
EK <eke00372@gmail.com>
2018-08-03 15:39:35 +0800

    ekchat: rewrite this

--- EKChat.lua
+++ EKChat.lua
@@ -10,30 +10,20 @@
 -- [[ config ]] --
 
 local font 			= STANDARD_TEXT_FONT
-local fontsize 		= 16
+local fontsize 		= 18
 local fontstyle 	= "OUTLINE"
 local bottomflash	= false		-- high cpu usage.
+local maxLines		= 1024
+local maxWidth, maxHeight = UIParent:GetWidth(), UIParent:GetHeight()
+
+-- [[ global ]] --
 
 local _G = _G
--- 更多字體大小
-_G.CHAT_FONT_HEIGHTS = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}
--- 頻道保持
-_G.ChatTypeInfo["SAY"].sticky = 1				-- 說
-_G.ChatTypeInfo["PARTY"].sticky = 1				-- 小隊
-_G.ChatTypeInfo["GUILD"].sticky = 1				-- 公會
-_G.ChatTypeInfo["WHISPER"].sticky = 0			-- 密語
-_G.ChatTypeInfo["BN_WHISPER"].sticky = 0		-- 戰網密語
-_G.ChatTypeInfo["RAID"].sticky = 1				-- 團隊
-_G.ChatTypeInfo["OFFICER"].sticky = 1			-- 幹部
-_G.ChatTypeInfo["CHANNEL"].sticky = 1			-- 頻道
-_G.ChatTypeInfo["INSTANCE_CHAT"].sticky = 1		-- 副本
-_G.ChatTypeInfo["YELL"].sticky = 0				-- 喊
-_G.ChatTypeInfo["BN_INLINE_TOAST_ALERT"].flashTab = false
-_G.ChatTypeInfo["BN_INLINE_TOAST_BROADCAST"].flashTab = false
-_G.ChatTypeInfo["BN_INLINE_TOAST_BROADCAST_INFORM"].flashTab = false
+
+
 -- 框架透明
-_G.DEFAULT_CHATFRAME_ALPHA = 0
-_G.DEFAULT_CHATFRAME_COLOR = {r = 0, g = 0, b = 0}
+--[[_G.DEFAULT_CHATFRAME_ALPHA = 0
+_G.DEFAULT_CHATFRAME_COLOR = {r = 0, g = 0, b = 0, a = 0}
 -- TAB淡出
 _G.CHAT_TAB_SHOW_DELAY = 0
 _G.CHAT_TAB_HIDE_DELAY = 5
@@ -44,10 +34,10 @@ _G.CHAT_FRAME_TAB_SELECTED_NOMOUSE_ALPHA = 0
 _G.CHAT_FRAME_TAB_ALERTING_MOUSEOVER_ALPHA = 1
 _G.CHAT_FRAME_TAB_ALERTING_NOMOUSE_ALPHA = 0.6
 _G.CHAT_FRAME_TAB_NORMAL_MOUSEOVER_ALPHA = 0.6
-_G.CHAT_FRAME_TAB_NORMAL_NOMOUSE_ALPHA = 0
+_G.CHAT_FRAME_TAB_NORMAL_NOMOUSE_ALPHA = 0]]--
 
 
--- [[ 頻道縮寫&名字引號 ]] --
+-- [[ 頻道縮寫 & 去除名字引號 ]] --
 
 local gsub = _G.string.gsub
 local newAddMsg = {}
@@ -76,126 +66,98 @@ local function DefaultCVar()
 	SetCVar("whisperMode", "inline")
 end
 
--- 全局的幹掉功能
-local function kill(frame)
-	if frame.UnregisterAllEvents then
-		frame:UnregisterAllEvents()
-	end
-	frame:Hide()
-	frame.Show = function() end
-end
-
+--改良toastframe
+local BNToastFrame = BNToastFrame
+BNToastFrame:SetClampedToScreen(true)
+BNToastFrame:SetClampRectInsets(-15, 15, 15, -15)
+BNToastFrame:HookScript("OnShow", function(self)
+	self:ClearAllPoints()
+	self:SetPoint("BOTTOMLEFT", ChatFrame1Tab, "TOPLEFT", 0, 25)
+end)
+	
 -- 框架優化
-local function SkinChat(self,event,...)
-	--幹掉通知視窗和快速加入
-	kill(ChatFrameMenuButton)
-	kill(QuickJoinToastButton)
-	--改良toastframe
-	BNToastFrame:SetClampedToScreen(true)
-	BNToastFrame:SetClampRectInsets(-15, 15, 15, -15)
-	BNToastFrame:HookScript("OnShow", function(self)
-		self:ClearAllPoints()
-		self:SetPoint("BOTTOMLEFT", ChatFrame1Tab, "TOPLEFT", 0, 25)
-	end)	
-	
-	for i = 1, NUM_CHAT_WINDOWS do
-	
-		-- chat
-		local ChatFrame = _G["ChatFrame" .. i]
-		ChatFrame1:SetUserPlaced(true)			-- 允許置底
-		-- 透明
-		FCF_SetWindowAlpha(ChatFrame, 0)
-		ChatFrame:SetSpacing(4)					-- 行距
-		-- 淡出
-		ChatFrame:SetFading(true)				-- 啟用淡出
-		ChatFrame:SetFadeDuration(2)			-- 淡出動畫持續時間
-		ChatFrame:SetTimeVisible(20)			-- 可見時間
-		-- 尺寸
-		ChatFrame:SetFrameLevel(8)				-- 框體層級
-		ChatFrame:SetClampedToScreen(false)		-- 固定在螢幕內
-		ChatFrame:SetClampRectInsets(0, 0, 0, 0)
-		ChatFrame:SetMinResize(128, 64)
-		ChatFrame:SetMaxResize(UIParent:GetWidth(), UIParent:GetHeight())
-		-- 文字
-		local _, size = ChatFrame:GetFont()
-		ChatFrame:SetFont(font, fontsize, fontstyle)
-		ChatFrame:SetShadowOffset(0, 0)			-- 陰影
-		ChatFrame:SetShadowColor(0, 0, 0, 0)
-		-- 幹掉材質
-		kill(_G["ChatFrame" .. i .. "ButtonFrame"])
-		kill(_G["ChatFrame" .. i .. "ButtonFrameBottomButton"])
-		kill(_G["ChatFrame" .. i .. "TabLeft"])
-		kill(_G["ChatFrame" .. i .. "TabRight"])
-		kill(_G["ChatFrame" .. i .. "TabMiddle"])
-		kill(_G["ChatFrame" .. i .. "TabSelectedLeft"])
-		kill(_G["ChatFrame" .. i .. "TabSelectedRight"])
-		kill(_G["ChatFrame" .. i .. "TabSelectedMiddle"])
-		kill(_G["ChatFrame" .. i .. "TabHighlightLeft"])
-		kill(_G["ChatFrame" .. i .. "TabHighlightRight"])
-		kill(_G["ChatFrame" .. i .. "TabHighlightMiddle"])
-		
-		-- tab
-		local Tab = _G["ChatFrame" .. i .. "Tab"]		
-		--local TabGlow = _G["ChatFrame".. i .."TabGlow"]
-		-- 初始透明度
-		Tab:SetAlpha(0)
-		Tab.noMouseAlpha = 0
-		-- 文字
-		local TabText = _G["ChatFrame" .. i .. "TabText"]
-		TabText.SetTextColor = function() end
-		TabText:SetFont(font, fontsize-2, fontstyle)
-		TabText:SetShadowOffset(0, 0)
-		TabText:SetShadowColor(0, 0, 0, 0)
-		
-		-- editbox
-		local EditBox = _G["ChatFrame" .. i .. "EditBox"]
-		local EditBoxHeader = _G["ChatFrame" .. i .. "EditBoxHeader"]
-		EditBox:SetAltArrowKeyMode(false)
-		--EditBox:EnableMouse(false)	-- 禁止滑鼠點擊		
-		-- 大小
-		EditBox:ClearAllPoints()
-		EditBox:SetPoint("BOTTOMLEFT", ChatFrame, "TOPLEFT", 6, 24)
-		EditBox:SetPoint("TOPRIGHT", ChatFrame, "TOPRIGHT", -30, 52)
-		-- 文字
-		EditBox:SetFont(font, fontsize, fontstyle)
-		EditBoxHeader:SetFont(font, fontsize, fontstyle)
-		-- 只留一個深色背景
-		EditBox:SetBackdrop(
-		{bgFile = [[Interface\Buttons\WHITE8X8]],
-		insets = { left = 2, right = 2, top = 2, bottom = 2 },}
-		)
-		EditBox:SetBackdropColor(0, 0, 0, 0.5)
-		--EditBox:SetBackdropBorderColor(0.3, 0.3, 0.30, 0.80)
-		EditBox:SetShadowOffset(0, 0)
-		-- 幹掉材質
-		kill(_G["ChatFrame" .. i .. "EditBoxMid"])
-		kill(_G["ChatFrame" .. i .. "EditBoxLeft"])
-		kill(_G["ChatFrame" .. i .. "EditBoxRight"])
-		--kill(_G["ChatFrame" .. i .. "EditBoxLanguage"])	-- 保留輸入法
-		kill(_G["ChatFrame" .. i .. "EditBoxFocusMid"])
-		kill(_G["ChatFrame" .. i .. "EditBoxFocusLeft"])
-		kill(_G["ChatFrame" .. i .. "EditBoxFocusRight"])
-		
-		--輸入法(中/英 字)
-		local lang = _G["ChatFrame" .. i .. "EditBoxLanguage"]
-		lang:GetRegions():SetAlpha(0)
-		lang:ClearAllPoints()
-		lang:SetPoint("TOPLEFT", EditBox, "TOPRIGHT", -2, 0)
-		lang:SetPoint("BOTTOMRIGHT", EditBox, "BOTTOMRIGHT", 28, 0)
-		lang:SetBackdrop(
-		{bgFile = [[Interface\Buttons\WHITE8X8]],
-		insets = { left = 2, right = 2, top = 2, bottom = 2 },}
-		)
-		lang:SetBackdropColor(0, 0, 0, 0.5)
-		--右鍵聊天選項小按鈕
-		--[[lang:HookScript("OnMouseUp", function(_, btn)
-			if btn == "RightButton" then
-				ChatMenu:ClearAllPoints()
-				ChatMenu:SetPoint("BOTTOMRIGHT", EditBox, 0, 30)
-				ToggleFrame(ChatMenu)
-			end
-		end)]]--
+local function skinChat(self)
+	if not self or (self and self.styled) then return end
+	local name = self:GetName()
+	-- chat
+	self:SetSpacing(4)					-- 行距
+	-- 淡出
+	self:SetFading(true)				-- 啟用淡出
+	self:SetFadeDuration(2)				-- 淡出動畫持續時間
+	self:SetTimeVisible(20)				-- 可見時間
+	-- 尺寸
+	self:SetFrameLevel(8)				-- 框體層級
+	self:SetClampedToScreen(false)		-- 固定在螢幕內
+	self:SetClampRectInsets(0, 0, 0, 0)
+	self:SetMinResize(128, 64)
+	self:SetMaxResize(maxWidth, maxHeight)
+	-- 文字
+	self:SetFont(font, fontsize, fontstyle)
+	self:SetShadowOffset(0, 0)			-- 陰影
+	self:SetShadowColor(0, 0, 0, 0)
+	-- 最大行數
+	if self:GetMaxLines() < maxLines then
+		self:SetMaxLines(maxLines)
 	end
+
+	-- editbox
+	local EditBox = _G[name.."EditBox"]
+	EditBox:SetAltArrowKeyMode(false)	
+	-- 大小
+	EditBox:ClearAllPoints()
+	EditBox:SetPoint("BOTTOMLEFT", self, "TOPLEFT", 6, 24)
+	EditBox:SetPoint("TOPRIGHT", self, "TOPRIGHT", -24, 56)
+	-- 只留一個深色背景
+	EditBox:SetBackdrop(
+	{bgFile = [[Interface\Buttons\WHITE8X8]],
+	insets = { left = 2, right = 2, top = 2, bottom = 2 },}
+	)
+	EditBox:SetBackdropColor(0, 0, 0, 0.5)
+	EditBox:SetShadowOffset(0, 0)
+	-- 幹掉材質
+	for i = 3, 8 do
+		select(i, EditBox:GetRegions()):SetAlpha(0)
+	end
+
+	--輸入法(中/英 字)
+	local lang = _G[name.."EditBoxLanguage"]
+	lang:GetRegions():SetAlpha(0)
+	lang:ClearAllPoints()
+	lang:SetPoint("TOPLEFT", EditBox, "TOPRIGHT", -2, 0)
+	lang:SetPoint("BOTTOMRIGHT", EditBox, "BOTTOMRIGHT", 28, 0)
+	lang:SetBackdrop(
+	{bgFile = [[Interface\Buttons\WHITE8X8]],
+	insets = { left = 2, right = 2, top = 2, bottom = 2 },}
+	)
+	lang:SetBackdropColor(0, 0, 0, 0.5)
+	--右鍵聊天選項小按鈕
+	lang:HookScript("OnMouseUp", function(_, btn)
+		if btn == "RightButton" then
+			ChatMenu:ClearAllPoints()
+			ChatMenu:SetPoint("BOTTOMRIGHT", EditBox, 0, 30)
+			ToggleFrame(ChatMenu)
+		end
+	end)
+			
+	-- tab
+	local Tab = _G[name.."Tab"]		
+	-- 初始透明度
+	Tab:SetAlpha(1)
+	Tab.noMouseAlpha = 0
+	-- 幹掉材質
+	for i = 1, 10 do
+		if i ~= 7 then -- skip msg highlight
+			select(i, Tab:GetRegions()):SetTexture(nil)
+		end
+	end		
+	-- 文字
+	local TabText = Tab:GetFontString()
+	TabText:SetTextColor(1, .8, 0)
+	TabText:SetFont(font, fontsize-2, fontstyle)
+	TabText:SetShadowOffset(0, 0)
+	TabText:SetShadowColor(0, 0, 0, 0)
+
+	self.styled = true
 end
 		
 -- [[ 未置底閃光(高cpu占用) ]] --
@@ -240,29 +202,50 @@ hooksecurefunc("FloatingChatFrame_OnMouseScroll", function(self, delta, ...)
 	end
 end)
 
--- [[ 動態視窗 ]] --
-
-local function TempChat()
-	local frame = FCF_GetCurrentChatFrame()
-	--關閉寵物戰鬥紀錄
-	if _G[frame:GetName().."Tab"]:GetText():match(PET_BATTLE_COMBAT_LOG) then
-		FCF_Close(frame)
-		else return
-	end
-end
-hooksecurefunc("FCF_OpenTemporaryWindow", TempChat)
-
 -- [[ 載入事件 ]] --
 
 local Event = CreateFrame("Frame", nil, UIParent)
 Event:RegisterEvent("PLAYER_ENTERING_WORLD")
---Event:RegisterEvent("PLAYER_LOGIN")
-Event:SetScript("OnEvent", function(self, event, unit, ...)
+Event:RegisterEvent("PLAYER_LOGIN")
+Event:SetScript("OnEvent", function(self, event, ...)
 	if event == "PLAYER_ENTERING_WORLD"  then
 		DefaultCVar()
-		SkinChat()
+		
+		for i = 1, NUM_CHAT_WINDOWS do
+			skinChat(_G["ChatFrame"..i])
+		end
+		-- 動態視窗(戰寵ETC)
+		hooksecurefunc("FCF_OpenTemporaryWindow", function()
+		for _, chatFrameName in next, CHAT_FRAMES do
+			local frame = _G[chatFrameName]
+			if frame.isTemporary then
+				skinChat(frame)
+			end
+		end
+	end)
+	
 		if bottomflash then
 			Flash()
 		else return end
 	end
+
+	for i = 1, 15 do
+		CHAT_FONT_HEIGHTS[i] = i + 9
+	end
+	
+	-- 頻道保持
+	ChatTypeInfo["SAY"].sticky = 1				-- 說
+	ChatTypeInfo["PARTY"].sticky = 1			-- 小隊
+	ChatTypeInfo["GUILD"].sticky = 1			-- 公會
+	ChatTypeInfo["WHISPER"].sticky = 0			-- 密語
+	ChatTypeInfo["BN_WHISPER"].sticky = 0		-- 戰網密語
+	ChatTypeInfo["RAID"].sticky = 1				-- 團隊
+	ChatTypeInfo["OFFICER"].sticky = 1			-- 幹部
+	ChatTypeInfo["CHANNEL"].sticky = 1			-- 頻道
+	ChatTypeInfo["INSTANCE_CHAT"].sticky = 1	-- 副本
+	ChatTypeInfo["YELL"].sticky = 0				-- 喊
+	-- 不要閃光
+	ChatTypeInfo["BN_INLINE_TOAST_ALERT"].flashTab = false
+	ChatTypeInfo["BN_INLINE_TOAST_BROADCAST"].flashTab = false
+	ChatTypeInfo["BN_INLINE_TOAST_BROADCAST_INFORM"].flashTab = false
 end)
